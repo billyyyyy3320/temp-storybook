0fe0e6c61573843bdbbff057eb01f23caaa4c395
Billyyyyy3320 <newsbielt703@gmail.com>
2020-08-04 18:03:36 +0800

    feat: init composition

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,4 +2,8 @@
 
 module.exports = {
   stories: ['../src/**/*.stories.js'],
+  refs:{
+    master:'https://tender-lovelace-dd9f22.netlify.app',
+    blabla:'https://blissful-allen-e1b86d.netlify.app'
+  }
 };
